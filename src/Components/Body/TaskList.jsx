2cee62bda7e5b0498603f6cb97940c071239cf5b
Alejandro <baudra21@gmail.com>
2023-11-20 13:14:28 -0300

    se agrega css para acomodar el diseño al original

--- src/Components/Body/TaskList.jsx
+++ src/Components/Body/TaskList.jsx
@@ -5,7 +5,7 @@ import './TaskList.css';
 const TaskList = () => {
   const tasks = [
     // Puedes llenar esta array con las tareas que quieras mostrar
-    { id: 1, description: 'Tarea 1' },
+    { id: 1, description: 'Throw away my books' },
     { id: 2, description: 'Tarea 2' },
     { id: 3, description: 'Tarea 3' },
     { id: 4, description: 'Tarea 4' }
@@ -14,12 +14,13 @@ const TaskList = () => {
 
   return (
     <div className="task-list-container">
+      <div className='vertical-line'></div>
       <div className="folder-sheet">
         <div className="empty-row"></div>
         {tasks.map((task) => (
           <div key={task.id} className="task-row">
-            <div className="left-column">
-               
+            <div className="left-column"> 
+                         
                 {/* Aquí puedes agregar el cuadrado para eventos */}
               <div className="event-square"></div>
             </div>
